fc44838b8aff5b884af0f5d8b431c29aac0bc7f8
Pavla Kratochvilova <pavla.kratochvilova@gmail.com>
2020-02-29 21:44:41 +0100

    Add only the first tone event into the buffer

--- trigger.py
+++ trigger.py
@@ -12,8 +12,19 @@ def make_pattern(sequence):
     for char in sequence:
         if char == ' ':
             char = ''
-        pattern += r'\[%s\](?:\[\])?' % char
+        pattern += r'%s(?: )?' % char
     return pattern
+
+def parse_line(valid_line, line, previous):
+    mo = valid_line.match(line)
+    chars = mo.group(1)
+    if chars is None:
+        chars = ""
+    if chars != "":
+        chars = chars.split(" ")
+    current = set(chars).difference(previous)
+    previous = set(chars)
+    return previous, current
 
 def main(*in_args):
     parser = argparse.ArgumentParser('Trigger event when specific sequence is read.')
@@ -36,12 +47,15 @@ def main(*in_args):
     args = parser.parse_args(in_args)
     pattern = make_pattern(args.sequence)
     regexp = re.compile(pattern)
+    valid_line = re.compile(r"\[([^\]]*)\]$")
     count = 0
     max_count = args.count
     if max_count < 0:
         max_count = float('inf')
     command = functools.partial(subprocess.Popen, args.command_arg)
     data = io.StringIO()
+    previous = set()
+    current = set()
     while True:
         if count >= max_count:
             if args.keep_reading:
@@ -51,7 +65,12 @@ def main(*in_args):
         line = sys.stdin.readline()
         if line == '':
             break
-        data.write(line.rstrip('\n'))
+        previous, current = parse_line(valid_line, line.rstrip('\n'), previous)
+        if len(previous) < 1 and len(current) < 1: # Two empty brackets in a row
+            data.write(" ")
+        else:
+            for c in sorted(list(current)):
+                data.write(c)
         #print('Data:', data.getvalue())
         if regexp.search(data.getvalue()):
             data = io.StringIO()
